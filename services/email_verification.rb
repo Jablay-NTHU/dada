a3e686935b3b924246e8d7220953f2c379d8ad98
bhimasta <agoengbhimasta@gmail.com>
2018-05-28 18:22:11 +0800

    modify email verification content

--- services/email_verification.rb
+++ services/email_verification.rb
@@ -1,12 +1,45 @@
 # frozen_string_literal: true
 
+require 'http'
+
 module Dada
+  # Error for invalid credentials
   class InvalidRegistration < StandardError; end
 
+  # Find account and check password
   class EmailVerification
-    # Send email verification
+    SENDGRID_URL = 'https://api.sendgrid.com/v3/mail/send'
+
+    def initialize(config)
+      @config = config
+    end
+
+    def username_available?(registration)
+      Account.first(username: registration[:username]).nil?
+    end
+
+    def email_body(registration)
+      verification_url = registration['verification_url']
+
+      <<~END_EMAIL
+        <p>Dear #{registration['username']}, </p>
+        <p>We received a request to create a new Dada account for you. </br>
+        To create a Dada account, please <a href=\"#{verification_url}\">click here</a></p>
+        <p>Best regards,</br>
+        Dada</p>
+        </br>
+        <hr>
+        <p>Please do not reply to this email. </br>
+        This email address is used only for sending email
+        so you will not receive a response. 
+      END_EMAIL
+    end
+
+    # rubocop:disable Metrics/MethodLength
     def send_email_verification(registration)
-      HTTP.auth("Bearer #{@config.SENDGRID_KEY}").post(
+      HTTP.auth(
+        "Bearer #{@config.SENDGRID_KEY}"
+      ).post(
         SENDGRID_URL,
         json: {
           personalizations: [{
@@ -14,16 +47,17 @@ module Dada
           }],
           from: { 'email' => 'noreply@dada.com' },
           subject: 'Dada Registration Verification',
-          content: [{
-            type: 'text/html',
-            value: email_body(registration)
-          }]
+          content: [
+            { type: 'text/html',
+              value: email_body(registration) }
+          ]
         }
       )
     rescue StandardError
       raise(InvalidRegistration,
-            'Could not send verification email; please check email ...')
+            'Could not send verification email; please check email address')
     end
+    # rubocop:enable Metrics/MethodLength
 
     def call(registration)
       raise(InvalidRegistration, 'Username already exists') unless
