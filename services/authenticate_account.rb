d4453193f3519428ac3c802208d8db784276f4d4
triwjy <ztriwijaya@gmail.com>
2018-05-31 12:14:18 +0800

    refactor securedb and issue-require tokens

--- services/authenticate_account.rb
+++ services/authenticate_account.rb
@@ -11,12 +11,13 @@ module Dada
       "Invalid Credentials for: #{@credentials[:username]}"
     end
   end
-
   # Find account and check password
   class AuthenticateAccount
     def self.call(credentials)
       account = Account.first(username: credentials[:username])
-      account.password?(credentials[:password]) ? account : raise
+      raise StandardError unless account.password?(credentials[:password])
+
+      { account: account, auth_token: AuthToken.create(account) }
     rescue StandardError
       raise UnauthorizedError, credentials
     end
