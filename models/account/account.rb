a8fd3a7d84cf91957f2d7874bcd52515d65b8fce
victorlin12345 <victorlin12345@gmail.com>
2018-06-18 17:17:09 +0800

    model account add profile_url to white list

--- models/account/account.rb
+++ models/account/account.rb
@@ -21,7 +21,7 @@ module Dada
                  left_key: :collaborator_id, right_key: :project_id
 
     plugin :whitelist_security
-    set_allowed_columns :username, :email, :password, :password_hash, :salt
+    set_allowed_columns :username, :email, :password, :password_hash, :salt, :profile_url
     # set_allowed_columns :username, :email, :password
 
     plugin :timestamps, update_on_create: true
